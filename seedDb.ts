9f10eed243916773f5fa2f69bbaac6fec9633103
Your Name <you@example.com>
2024-06-03 01:23:39 +0000

    Added SQLite database and seeding script for initial data population

---
+++ seedDb.ts
@@ -0,0 +1,56 @@
+import initSqlJs from "sql.js";
+import { drizzle } from "drizzle-orm/sql-js";
+import fs from "fs/promises";
+import * as schema from "./src/schema.ts";
+
+export async function loadDatabase(path: string) {
+  const SQL = await initSqlJs({
+    locateFile: (file) => `./public/${file}`,
+  });
+
+  let buffer: Buffer | null = null;
+  let shouldInit = false;
+
+  try {
+    buffer = await fs.readFile(path);
+  } catch {
+    shouldInit = true;
+  }
+
+  const database = new SQL.Database(buffer);
+  const db = drizzle(database, { schema });
+
+  async function save() {
+    const exportBuffer = database.export();
+    await fs.writeFile(path, exportBuffer);
+  }
+
+  if (shouldInit) {
+    try {
+      const initialMigrationSql = await fs.readFile(
+        "./drizzle/0000_luxuriant_johnny_blaze.sql",
+        "utf8"
+      );
+
+      database.exec(initialMigrationSql);
+      await save();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  return {
+    db,
+    save,
+  };
+}
+
+
+console.log("⌛ Seeding database...");
+const { db, save } = await loadDatabase("./public/sqlite.db");
+
+await db.insert(schema.users).values({ name: "John" });
+await db.insert(schema.users).values({ name: "Jane" });
+
+await save();
+console.log("✅ Database seeded.");
